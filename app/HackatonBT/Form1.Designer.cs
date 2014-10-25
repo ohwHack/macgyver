e039b86d1ebd42da2a39ca4406b759674eb127bc
ah01 <horcicaa@gmail.com>
2014-10-25 19:51:06 +0200

    App for driving

---
+++ app/HackatonBT/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace HackatonBT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbJoystick = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.cbPort = new System.Windows.Forms.ComboBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pbRadar = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRadar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort
+            // 
+            this.serialPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort_DataReceived);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbJoystick);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.cbPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(259, 94);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Připojení";
+            // 
+            // cbJoystick
+            // 
+            this.cbJoystick.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbJoystick.FormattingEnabled = true;
+            this.cbJoystick.Location = new System.Drawing.Point(26, 23);
+            this.cbJoystick.Name = "cbJoystick";
+            this.cbJoystick.Size = new System.Drawing.Size(202, 21);
+            this.cbJoystick.TabIndex = 3;
+            this.cbJoystick.DropDown += new System.EventHandler(this.cbJoystick_DropDown);
+            this.cbJoystick.SelectedIndexChanged += new System.EventHandler(this.cbJoystick_SelectedIndexChanged);
+            this.cbJoystick.TextUpdate += new System.EventHandler(this.cbJoystick_TextUpdate);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(153, 50);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Připojit";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // cbPort
+            // 
+            this.cbPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbPort.FormattingEnabled = true;
+            this.cbPort.Location = new System.Drawing.Point(26, 50);
+            this.cbPort.Name = "cbPort";
+            this.cbPort.Size = new System.Drawing.Size(121, 21);
+            this.cbPort.TabIndex = 1;
+            this.cbPort.DropDown += new System.EventHandler(this.cbPort_DropDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // pbRadar
+            // 
+            this.pbRadar.Location = new System.Drawing.Point(508, 181);
+            this.pbRadar.Name = "pbRadar";
+            this.pbRadar.Size = new System.Drawing.Size(197, 179);
+            this.pbRadar.TabIndex = 5;
+            this.pbRadar.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(253, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 372);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pbRadar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbRadar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox cbPort;
+        private System.Windows.Forms.ComboBox cbJoystick;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pbRadar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ app/HackatonBT/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SlimDX;
+using SlimDX.DirectInput;
+using System.Globalization;
+
+namespace HackatonBT
+{
+
+    
+
+    public partial class Form1 : Form
+    {
+        public class GamepadDevice
+        {
+            public Guid Guid { get; set; }
+            public string Name { get; set; }
+
+            public override string ToString()
+            {
+                return Name;
+            }
+        }
+
+        Joystick pad;
+        GamepadDevice Device;
+
+        Bitmap img;
+
+        int pos = 70;
+
+        IList<GamepadDevice> devices;
+
+        Queue<string> data = new Queue<string>();
+        object data_lock = new object();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            img = new Bitmap(pbRadar.Width, pbRadar.Height);
+
+            Available();
+        }
+
+        private void acquire(System.Windows.Forms.Form parent)
+        {
+            DirectInput dinput = new DirectInput();
+
+            pad = new Joystick(dinput, this.Device.Guid);
+            foreach (DeviceObjectInstance doi in pad.GetObjects(ObjectDeviceType.Axis))
+            {
+                pad.GetObjectPropertiesById((int)doi.ObjectType).SetRange(-5000, 5000);
+            }
+
+            pad.Properties.AxisMode = DeviceAxisMode.Absolute;
+            pad.SetCooperativeLevel(parent, (CooperativeLevel.Nonexclusive | CooperativeLevel.Background));
+            pad.Acquire();
+        }
+
+        private void cbPort_DropDown(object sender, EventArgs e)
+        {
+            cbPort.DataSource = SerialPort.GetPortNames();
+        }
+
+        public virtual IList<GamepadDevice> Available()
+        {
+            IList<GamepadDevice> result = new List<GamepadDevice>();
+            DirectInput dinput = new DirectInput();
+            foreach (DeviceInstance di in dinput.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                GamepadDevice dev = new GamepadDevice();
+                dev.Guid = di.InstanceGuid;
+                dev.Name = di.InstanceName;
+                //Console.WriteLine(di.InstanceName) ;
+                result.Add(dev);
+            }
+            return result;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+                btnConnect.Text = "Připojit";
+            }
+            else
+            {
+                if (cbPort.Text != "")
+                {
+                    serialPort.PortName = cbPort.Text;
+                    serialPort.Open();
+                    
+
+                    btnConnect.Text = "Odpojit";
+                }
+            }
+
+
+        }
+
+        private void cbJoystick_DropDown(object sender, EventArgs e)
+        {
+            devices = Available();
+
+            cbJoystick.DataSource = devices;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (Device != null)
+            {
+                JoystickState state = new JoystickState();
+
+                if (pad.Poll().IsFailure)
+                {
+                    Console.WriteLine("fail");
+                    return;
+                }
+
+                if (pad.GetCurrentState(ref state).IsFailure)
+                {
+                    Console.WriteLine("fail2");
+                    return;
+                }
+
+                float speed = ((float)state.Z / -5000);
+                float directionA = ((float)state.X / 5000);
+                float directionB = ((float)state.RotationX / -5000);
+
+
+                if (speed < 0)
+                {
+                    directionA *= -1;
+                }
+
+                if (Math.Abs(directionB) < 0.2)
+                {
+                    directionB = 0;
+                }
+
+                if (Math.Abs(directionA) < 0.4)
+                {
+                    directionA = 0;
+                }
+
+                if (Math.Abs(speed) < 0.2)
+                {
+                    speed = 0;
+                }
+
+                //pbSpeed.Value = (int)Math.Abs(speed * 100);
+                //pbSpeed.ForeColor = (speed < 0 ? Color.Blue : Color.Red);
+
+                bool[] buttons = state.GetButtons();
+                bool prava = buttons[1];
+                bool leva = buttons[2];
+                bool reset = buttons[3];
+
+                if (prava)
+                {
+                    pos -= 5;
+                }
+                else if (leva)
+                {
+                    pos += 5;
+                }
+
+                if (reset)
+                {
+                    pos = 70;
+                }
+
+                if (serialPort.IsOpen)
+                {
+                    if (directionA != 0 && speed == 0)
+                    {
+                        float l = directionA;
+                        float r = directionA * -1;
+                        string cmd = "M " + l.ToString("F", CultureInfo.InvariantCulture) + " " + r.ToString("F", CultureInfo.InvariantCulture) + "\n";
+                        serialPort.WriteLine(cmd);
+                        Console.WriteLine(cmd);
+                    }
+                    else if (speed != 0)
+                    {
+                        string cmd = "R " + directionB.ToString("F", CultureInfo.InvariantCulture) + " " + speed.ToString("F", CultureInfo.InvariantCulture) + "\n";
+                        serialPort.WriteLine(cmd);
+                        Console.WriteLine(cmd);
+                    }
+                    else
+                    {
+                        string cmd = "M 0 0\n";
+                        serialPort.WriteLine(cmd);
+                        Console.WriteLine(cmd);
+                        cmd = "R 0 0\n";
+                        serialPort.WriteLine(cmd);
+                        Console.WriteLine(cmd);
+                    }
+
+                    string cmds = "C " + pos + "\n";
+                    serialPort.WriteLine(cmds);
+                    Console.WriteLine(cmds);
+                }
+
+                label1.Text = speed.ToString("F", CultureInfo.InvariantCulture);
+                label2.Text = directionA.ToString("F", CultureInfo.InvariantCulture);
+                label3.Text = directionB.ToString("F", CultureInfo.InvariantCulture);
+                
+                
+
+
+            }
+
+            drawData();
+
+
+        }
+
+        private void cbJoystick_TextUpdate(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cbJoystick_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Device = (GamepadDevice)cbJoystick.SelectedItem;
+            acquire(this);
+        }
+
+        private void drawData()
+        {
+            
+            while (true)
+            {
+                string line = "";
+                lock (data_lock)
+                {
+                    if (data.Count > 0)
+                    {
+                        line = data.Dequeue();
+                    }
+                }
+                if (line == "")
+                {
+                    break;
+                }
+
+                string[] parts = line.Split(' ');
+
+                int width = img.Width;
+                int height = img.Height;
+
+                if (parts.Length == 3 && parts[0] == "S")
+                {
+
+                    int degree = Convert.ToInt32(parts[1]);
+                    double f = Convert.ToDouble(parts[2].Replace("\r", ""), CultureInfo.InvariantCulture);
+
+                    int x = Math.Min((degree * width) / 100, width - 1);
+                    int y = Math.Min((((int)f * height) / 300), height - 1);
+
+                    for (int i = 0; i < img.Height; i++)
+                    {
+                        img.SetPixel(x, height - i - 1, (i < y ? Color.Black : Color.White));
+                    }
+                    pbRadar.Image = img;
+
+                    //Console.WriteLine(degree + " " + f);
+
+                }
+                else if (parts.Length == 2 && parts[0] == "A")
+                {
+                    label4.Text = parts[1];
+                }
+                else
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        private void serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            lock (data_lock)
+            {
+                if (serialPort.IsOpen)
+                {
+                    data.Enqueue(serialPort.ReadLine());
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+        }
+
+    }
+}
